1c1eb9881d5441e82279e61ec4f0d8b5dc5ae079
DGwebdes <alan.bilik8@gmail.com>
2025-08-15 03:46:39 +0100

    updating proxy api url

--- part2/phonebook/vite.config.js
+++ part2/phonebook/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": {
-                target: "http://localhost:3001",
+                target: "https://okies-c5ee.onrender.com:10000",
                 changeOrigin: true,
             },
         },
